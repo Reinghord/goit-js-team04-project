9ffdf02fec2dbf42dae85b2c1913da6fb74b9183
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-08-17 18:49:11 +0300

    code optimization and fixes

--- src/index.js
+++ src/index.js
@@ -9,18 +9,22 @@ import getCocktails from './js/thecocktailsDB';
 const { cocktailsList } = {
   cocktailsList: document.querySelector('.cocktails-list'),
 };
+////Var to store markup data on init loading or query
 let markup = [];
+////Var to control listener on window for resize event
+const debounceResizedMarkup = debounce(resizeMarkup, 200);
 
 //!!! Initial page loading !!!
+onLoadingHome();
 
-//Renders random cocktails
-getAndRenderRandomCocktails();
-//Captures changes to window size to render 3, 6 or 9 items
-//Using debounce to prevent chatty events
-window.addEventListener('resize', debounce(debounceMarkup, 200));
+//Function to execute on initial page loading OR when returned to Home window for SPA
+function onLoadingHome() {
+  getAndRenderRandomCocktails();
+  window.addEventListener('resize', debounceResizedMarkup);
+}
 
 //Function to update markup amount based on window width
-function debounceMarkup() {
+function resizeMarkup() {
   cocktailsList.innerHTML = markupFilter(markup);
 }
 
