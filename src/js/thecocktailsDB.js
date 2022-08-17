1e4af4a53b8e119b50b4d91e8af7b7c2cc85a7e5
Varden41 <arturpavlenko14@gmail.com>
2022-08-17 14:21:00 +0300

    Update thecocktailsDB.js

--- src/js/thecocktailsDB.js
+++ src/js/thecocktailsDB.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-export default async function getCocktails(request) {
+export default async function getCocktails() {
   try {
-    const URL = 'www.thecocktaildb.com/api/json/v1/1/search.php';
+    const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
     const searchParams = new URLSearchParams({
-      key: '1',
+      f: 'a',
     });
-    const searchResponse = await axios.get(`${URL}/?${searchParams}`);
+    const searchResponse = await axios.get(`${BASE_URL}/?${searchParams}`);
     console.log(searchResponse);
     return searchResponse;
   } catch (error) {
     throw new Error(error);
   }
 }
+
+getCocktails();
