061b595d2d6f0c769674318d4fc1a7cc408ef372
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-08-19 18:34:14 +0300

    added add to fav and remove from favs

--- src/js/firebase/addToFavourite.js
+++ src/js/firebase/addToFavourite.js
@@ -1,9 +1,21 @@
-import { addToFavourite } from '../../service/firebase';
-import { cocktailsList } from '../refs';
+// import { getAuth } from 'firebase/auth';
+// import { addToFavourite, removeFromFavourite } from '../../service/firebase';
+// import { errorNoLogin } from '../notifications';
+// import { cocktailsList } from '../refs';
 
-cocktailsList.addEventListener('click', e => {
-  if (e.target.dataset.action === 'favourite') {
-    const id = e.target.parentElement.parentElement.id;
-    addToFavourite(id);
-  }
-});
+// cocktailsList.addEventListener('click', e => {
+//   if (e.target.dataset.action === 'favourite') {
+//     const id = e.target.parentElement.parentElement.id;
+//     addToFavourite(id);
+//     e.target.dataset.action = 'addedToFavourite';
+
+//     errorNoLogin();
+//     return;
+//   }
+//   if (e.target.dataset.action === 'addedToFavourite') {
+//     const id = e.target.parentElement.parentElement.id;
+//     removeFromFavourite(id);
+//     e.target.dataset.action = 'favourite';
+//     return;
+//   }
+// });
