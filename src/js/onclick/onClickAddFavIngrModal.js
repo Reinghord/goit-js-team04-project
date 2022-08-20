04521c41c8558987396c0b91275dd2b7bb588aea
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-08-20 19:30:38 +0300

    working on new feature

---
+++ src/js/onclick/onClickAddFavIngrModal.js
@@ -0,0 +1,62 @@
+// import { auth } from '../../service/firebase';
+// import { ref, getDatabase, child } from 'firebase/database';
+// import { errorNoLogin } from '../notifications';
+// import { addToFavouriteIngr } from '../../service/firebase';
+// import { getFavouriteCocktails } from '../../service/firebase';
+// import { removeFromFavouriteIngr } from '../../service/firebase';
+// import { backdrop } from '../refs';
+
+// backdrop.addEventListener('click', onClickIngr);
+
+// export function onClickIngr(e) {
+//   const id = e.target.dataset.id;
+//   if (e.target.dataset.action === 'favouriteIngr') {
+//     if (auth.currentUser) {
+//       addToFavouriteIngr(id);
+//       //   getFavouriteCocktails(renderFavouriteCocktailsIconModal);
+//       e.target.dataset.action = 'addedToFavouriteIngr';
+//       return;
+//     }
+//     errorNoLogin();
+//   }
+//   if (e.target.dataset.action === 'addedToFavouriteIngr') {
+//     if (auth.currentUser) {
+//       removeFromFavouriteIngr(id);
+//       e.target.firstElementChild.classList.remove('cocktails-svg--fav');
+//       e.target.dataset.action = 'favouriteIngr';
+//       return;
+//     }
+//     errorNoLogin();
+//   }
+// }
+
+// export function renderFavouriteCocktailsIconModal(snapshot) {
+//   {
+//     if (snapshot.exists()) {
+//       const data = snapshot.val();
+//       const dataKeys = Object.keys(data);
+//       dataKeys.forEach(id => {
+//         const query = document.querySelector(`[data-id="${id}"]`);
+//         if (query) {
+//           const svg = query.firstElementChild;
+//           query.dataset.action = 'addedToFavourite';
+//           svg.classList.add('cocktails-svg--fav');
+//         }
+//       });
+//     } else {
+//       return;
+//     }
+//   }
+// }
+
+// function getFavouriteIngredients(executeFunction) {
+//   if (auth.currentUser) {
+//     const userId = auth.currentUser.uid;
+//     const dbRef = ref(getDatabase());
+//     get(child(dbRef, `favourite/${userId}/ingredients`))
+//       .then(snapshot => executeFunction(snapshot))
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   }
+// }
