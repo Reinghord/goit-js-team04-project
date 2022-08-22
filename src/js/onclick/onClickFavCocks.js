afbe5026cb283505970193a4b20a910f8e1df248
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2022-08-22 22:11:35 +0300

    fix

--- src/js/onclick/onClickFavCocks.js
+++ src/js/onclick/onClickFavCocks.js
@@ -10,13 +10,15 @@ import {
 import { getCocktailById } from '../thecocktailsDB';
 import * as icons from '../../images/icons.svg';
 import { noResultsMarkup } from '../cocktails-markup';
-import { onChange } from '../header';
+import { cocktailByName, DEBOUNCE_DELAY, onChange } from '../header';
 import { errorNoLogin } from '../notifications';
+import debounce from 'lodash.debounce';
 
 btnFavCoks.addEventListener('click', async () => {
   try {
     await getFavouriteCocktails(onClickFavCocks);
     searchForm.removeEventListener('input', onChange);
+    searchForm.addEventListener('input', onChangeFilteredByName);
     sectionHero.style.display = 'none';
     titleCocktails.textContent = 'Favourite Cocktails';
     btnLoadMore.classList.add('btn_hidden');
@@ -33,7 +35,6 @@ function onClickFavCocks(snapshot) {
     Promise.all(get).then(response => {
       const filteredResponse = response.map(elem => elem.data.drinks[0]);
       const markup = cocktailsMarkup(filteredResponse);
-
       cocktailsList.innerHTML = markup;
     });
   } else {
@@ -46,7 +47,7 @@ function onClickFavCocks(snapshot) {
 const cocktailsMarkup = function (cocktailsData) {
   return cocktailsData
     .map(drink => {
-      return `<li class="cocktails__item" id="${drink.idDrink}" >
+      return `<li class="cocktails__item" id="${drink.idDrink}">
   <img class="cocktails__img" src="${drink.strDrinkThumb}" alt="${drink.strDrink}" />
   <p class="cocktails__name">${drink.strDrink}</p>
 <div class="btn__wrapper">  
@@ -60,3 +61,40 @@ const cocktailsMarkup = function (cocktailsData) {
     })
     .join('');
 };
+
+// search by Name
+
+const getFavouriteNameCocktails = e => {
+  const liElement = cocktailByName[0].childNodes;
+  const newArray = [];
+  const lastElem = cocktailByName[0].childNodes.length - 1;
+  liElement[lastElem].classList.add('hide-my__li');
+  for (let i = 0; i <= liElement.length; i++) {
+    const pElement = liElement[i];
+    const pElementValue = pElement.childNodes[3];
+    const normalizeElementValue = pElementValue.textContent.toLocaleLowerCase();
+    liElement[i].classList.remove('hide-my__li');
+    if (normalizeElementValue.includes(e)) {
+      cocktailByName.innerHTML = liElement[i];
+    } else if (!normalizeElementValue.includes(e)) {
+      liElement[i].classList.toggle('hide-my__li');
+      newArray.push(Object(liElement[i]));
+      if (newArray.length >= cocktailByName[0].childNodes.length) {
+        titleCocktails.textContent = `Sorry, we didn't find any cocktail for you`;
+        cocktailsList.insertAdjacentHTML('beforeend', noResultsMarkup());
+        liElement[lastElem + 1].classList.remove('hide-my__li');
+      }
+    }
+  }
+};
+
+export const onChangeFilteredByName = debounce(e => {
+  e.preventDefault();
+  try {
+    const value = e.target.value.toLocaleLowerCase().trim();
+    const res = getFavouriteNameCocktails(value);
+    console.log(value, res);
+  } catch (error) {
+    // errorNoLogin();
+  }
+}, DEBOUNCE_DELAY);
